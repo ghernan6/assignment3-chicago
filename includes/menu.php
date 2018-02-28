884464aab4bcee391e920b81d25e26c0e5f83df9
Andrew Regelski <aregelsk0113@me.com>
2018-02-28 16:20:36 -0500

    Slideshow, menu bar (w/o highlighting - if u can figure it out, go for it), php includes

---
+++ includes/menu.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="styles/styles.css">
+
+<nav class="menu">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="student1.php">Bei Lu</a></li>
+        <li><a href="student2.php">Conley Ernst</a></li>
+        <li><a href="student3.php">Jovan Lee</a></li>
+    </ul>
+</nav>
